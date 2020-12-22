c59e17c53038394e7b6723a0ef42cd901a0edc89
Tipper <tipper.langseth@gmail.com>
2020-12-21 18:06:55 -0600

    random commits before leaving for Rochester

--- 2017/day2.py
+++ 2017/day2.py
@@ -1,28 +1,30 @@
 lines = list()
-checksum = 0
 with open('day2.txt', 'r') as f:
     lines = f.readlines()
     f.close()
 
+checksum = 0
+divisionsum = 0
 for line in lines:
     numbers = line.split("\t")
-    breakI = False
     min = 1000000000
     max = 0
+    found_divisor = False
     for i in range(len(numbers)):
-
-        # num1 = int(numbers[i])
-        # for j in range(len(numbers)):
-        #     if (i == j):
-        #         continue
-        #     num2 = int(numbers[j])
-        #     test = num1 / num2
-        #     if (test == int(test)):
-        #         checksum += test
-        #         breakI = True
-        #         break
-        # if (breakI):
-        #     break
+        n = int(numbers[i])
+        if n < min:
+            min = n
+        if n > max:
+            max = n
+        if not found_divisor:
+            for j in range(len(numbers)):
+                n2 = int(numbers[j])
+                if n != n2 and n % n2 == 0:
+                    divisionsum += (n / n2)
+                    found_divisor = True
+                    break
+    checksum += (max - min)
 
 print(checksum)
+print(divisionsum)
 
---
+++ 2020/day20.py
@@ -0,0 +1,6 @@
+class PuzzlePiece:
+    def __init__(self, id):
+        self.id = id
+        self.data = list()
+
+    def h_flip(self):
